4947a3fbd95abcc2b6b4e216d2d24a50cd93dc22
thomasverweij <10090261+thomasverweij@users.noreply.github.com>
2026-01-29 15:44:43 +0100

    add github actions

---
+++ github-actions/playwright.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html', { open: 'never' }],
+    ['json', { outputFile: 'test-results/results.json' }],
+    ['list']
+  ],
+  use: {
+    baseURL: 'https://playwright.dev',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
